635d77bfcd8974c711e67688148944370c3135f2
Aredrom <sadaos2.97@gmail.com>
2022-11-27 00:15:22 +0100

    adding number validation logic and address model

---
+++ RandomProject/Models/Address.cs
@@ -0,0 +1,29 @@
+﻿namespace RandomProject.Models
+{
+    public class Address
+    {
+        public int id { get; set; }
+        public string uid { get; set; }
+        public string city { get; set; }
+        public string street_name { get; set; }
+        public string street_address { get; set; }
+        public string secondary_address { get; set; }
+        public string building_number { get; set; }
+        public string mail_box { get; set; }
+        public string community { get; set; }
+        public string zip_code { get; set; }
+        public string zip { get; set; }
+        public string postcode { get; set; }
+        public string time_zone { get; set; }
+        public string street_suffix { get; set; }
+        public string city_suffix { get; set; }
+        public string city_prefix { get; set; }
+        public string state { get; set; }
+        public string state_abbr { get; set; }
+        public string country { get; set; }
+        public string country_code { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public string full_address { get; set; }
+    }
+}
--- RandomProject/Program.cs
+++ RandomProject/Program.cs
@@ -2,6 +2,22 @@
 {
     internal class Program
     {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Type number between 5 and 20:");
 
+            string UserInput = "";
+            string numberString = Console.ReadLine();
+            int number = Int16.Parse(numberString);
+            if (number >= 5 && number <= 20)
+            {
+                UserInput = numberString;
+            }
+            else
+            {
+                Console.WriteLine("Number must be in the range from 5 to 20!");
+                Environment.Exit(0);
+            }
+        }
     }
 }
