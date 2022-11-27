445e7c6c11197170aa55f3684b35bf3250d5cbe7
Aredrom <sadaos2.97@gmail.com>
2022-11-27 11:57:59 +0100

    connecting to api and pulling user-defined number of random countries

--- RandomProject/Program.cs
+++ RandomProject/Program.cs
@@ -1,4 +1,9 @@
-﻿namespace RandomProject
+﻿using RandomProject.Models;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Newtonsoft.Json.Linq;
+
+namespace RandomProject
 {
     internal class Program
     {
@@ -17,6 +22,24 @@
             {
                 Console.WriteLine("Number must be in the range from 5 to 20!");
                 Environment.Exit(0);
+            }
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://random-data-api.com/api/v2/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            string resource = "addresses";
+            string sizeNumber = "?size=";
+
+            var response = client.GetAsync($"{resource}{sizeNumber}{UserInput}");
+            var result = response.Result;
+            result.EnsureSuccessStatusCode();
+
+            var content = await result.Content.ReadFromJsonAsync<List<Address>>();
+            Console.WriteLine();
+            foreach (var address in content)
+            {
+                Console.WriteLine(JToken.FromObject(address));
             }
         }
     }
