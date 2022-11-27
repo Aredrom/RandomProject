c836fa9728e1346efa857b9a371b154f14e0dddc
Aredrom <sadaos2.97@gmail.com>
2022-11-27 12:16:31 +0100

    adding try catch to prevent crashes and adding comments

--- RandomProject/Program.cs
+++ RandomProject/Program.cs
@@ -11,35 +11,48 @@ namespace RandomProject
         {
             Console.WriteLine("Type number between 5 and 20:");
 
-            string UserInput = "";
-            string numberString = Console.ReadLine();
-            int number = Int16.Parse(numberString);
-            if (number >= 5 && number <= 20)
+            try
             {
-                UserInput = numberString;
+                string UserInput = "";
+                string numberString = Console.ReadLine();
+
+                // parsing user input to int and validating if its from 5 to 20
+                int number = Int16.Parse(numberString);
+                if (number >= 5 && number <= 20)
+                {
+                    UserInput = numberString;
+                }
+                else
+                {
+                    Console.WriteLine("Number must be in the range from 5 to 20!");
+                    Environment.Exit(0);
+                }
+
+                // base api address
+                var client = new HttpClient();
+                client.BaseAddress = new Uri("https://random-data-api.com/api/v2/");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                // variables for creating user defined request
+                string resource = "addresses";
+                string sizeNumber = "?size=";
+
+                // connecting to api
+                var response = client.GetAsync($"{resource}{sizeNumber}{UserInput}");
+                var result = response.Result;
+                result.EnsureSuccessStatusCode();
+
+                // assigning given data to variable content and displaying on screen
+                var content = await result.Content.ReadFromJsonAsync<List<Address>>();
+                Console.WriteLine();
+                foreach (var address in content)
+                {
+                    Console.WriteLine(JToken.FromObject(address));
+                }
             }
-            else
+            catch(Exception ex)
             {
-                Console.WriteLine("Number must be in the range from 5 to 20!");
-                Environment.Exit(0);
-            }
-
-            var client = new HttpClient();
-            client.BaseAddress = new Uri("https://random-data-api.com/api/v2/");
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            string resource = "addresses";
-            string sizeNumber = "?size=";
-
-            var response = client.GetAsync($"{resource}{sizeNumber}{UserInput}");
-            var result = response.Result;
-            result.EnsureSuccessStatusCode();
-
-            var content = await result.Content.ReadFromJsonAsync<List<Address>>();
-            Console.WriteLine();
-            foreach (var address in content)
-            {
-                Console.WriteLine(JToken.FromObject(address));
+                Console.WriteLine(ex.Message);
             }
         }
     }
